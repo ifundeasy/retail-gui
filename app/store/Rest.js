c6413ac2ded367eebc78fbd2645e396af131d498
Afa Rappresent <mail.vudin@gmail.com>
2018-01-08 17:58:05 +0700

    let dirty store field before sync to send only original field on db table

--- app/store/Rest.js
+++ app/store/Rest.js
@@ -1,4 +1,19 @@
 Ext.define('Axp.store.Rest', {
     extend: 'Ext.data.Store',
-    model: 'Axp.model.Rest'
+    model: 'Axp.model.Rest',
+    listeners: {
+        beforesync: function (options, eOpts) {
+            let writerCols = this.writer;
+            for (let mod in options) {
+                let list = options[mod];
+                list.forEach(function (obj) {
+                    let data = {_: obj.data._};
+                    writerCols.forEach(function (el) {
+                        data[el.name] = obj.data[el.name] || '';
+                    });
+                    obj.data = data;
+                })
+            }
+        }
+    }
 });
