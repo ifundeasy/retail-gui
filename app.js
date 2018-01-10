21aec3faf574070954fc667890f6c9ae026f4ed5
Afa Rappresent <mail.vudin@gmail.com>
2018-01-10 17:37:12 +0700

    change messagebox when alerting

--- app.js
+++ app.js
@@ -1,23 +1,8 @@
-Ext.app.Application.prototype.addController = function (classPath, opts) {
-    let self = this,
-        config = opts || {};
-
-    Ext.require(classPath, function () {
-        let applier = Ext.apply({
-            application: self
-        }, config.options || {});
-        let controller = Ext.create(classPath, applier);
-
-        self.controllers.add(classPath, controller);
-
-        controller.init();
-
-        if (config.callback) {
-            config.callback.call((config.scope || this), config, controller)
-        }
-    });
-};
-(function () {
+Ext.require([
+    'Ext.window.MessageBox',
+    'Ext.tip.*'
+]);
+Ext.onReady(function () {
     let ajax = function (config = {}) {
         let request = $.ajax(Object.assign(config, {async: false}));
         let {status, statusText} = request;
@@ -31,25 +16,44 @@ Ext.app.Application.prototype.addController = function (classPath, opts) {
         backend = backendReq.response;
 
     if (!backend) {
-        window.alert(backendReq.statusText);
-        return;
+        Ext.Msg.show({
+            title: 'Error',
+            msg: backendReq.statusText,
+            buttons: Ext.Msg.OK,
+            icon: Ext.Msg.ERROR
+        });
+    } else {
+        let {protocol, ip, port, name} = backend;
+        backend.origin = `${protocol + ip}:${port}`;
+
+        Ext.app.Application.prototype.addController = function (classPath, opts) {
+            let self = this, config = opts || {};
+
+            Ext.require(classPath, function () {
+                let applier = Ext.apply({
+                    application: self
+                }, config.options || {});
+                let controller = Ext.create(classPath, applier);
+
+                self.controllers.add(classPath, controller);
+
+                controller.init();
+
+                if (config.callback) {
+                    config.callback.call((config.scope || this), config, controller)
+                }
+            });
+        };
+        
+        Ext.application({
+            name: 'A',
+            appFolder: 'app',
+            controllers: ['Core'],
+            version: base.version,
+            title: name,
+            enableQuickTips: true,
+            backend, ajax,
+            autoCreateViewport: true
+        });
     }
-
-    let {protocol, ip, port, name} = backend;
-    backend.origin = `${protocol + ip}:${port}`;
-
-    Ext.application({
-        name: 'A',
-        appFolder: 'app',
-        controllers: ['Core'],
-        version: base.version,
-        title: name,
-        enableQuickTips: true,
-        backend, ajax,
-        autoCreateViewport: true,
-        onAddController: function (index, controller, key) {
-            console.log('onAddController?');
-            controller.init();
-        }
-    });
-})();
+});
