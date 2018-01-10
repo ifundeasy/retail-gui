da6c3698cb0e71ff2363931d2250a8b204a8e0b9
Afa Rappresent <mail.vudin@gmail.com>
2018-01-10 23:26:32 +0700

    views have store requirement; using initComponent for very nested component

--- app/view/master/Media.js
+++ app/view/master/Media.js
@@ -1,130 +1,138 @@
-(function () {
-    let store = Ext.create('A.store.Media');
+Ext.require([
+    'A.store.Media',
+    'A.store.Status',
+]);
+Ext.define('A.view.master.Media', {
+    extend: 'Ext.panel.Panel',
+    alias: 'widget.masterMedia',
+    initComponent: function () {
+        let store = Ext.create('A.store.Media');
 
-    return Ext.define('A.view.master.Media', {
-        extend: 'Ext.panel.Panel',
-        alias: 'widget.masterMedia',
-        items: [
-            {
-                store,
-                xtype: 'grid',
-                loadMask: true,
-                selModel: {
-                    selType: 'checkboxmodel', //'Ext.selection.CheckboxModel'
-                    checkOnly: true,
-                    mode: 'MULTI'
-                },
-                columns: [
-                    //new Ext.grid.RowNumberer(),
-                    {text: 'ID', dataIndex: 'id', minWidth: 50, autoSizeColumn: true},
-                    {
-                        text: 'Name',
-                        dataIndex: 'name',
-                        minWidth: 100,
-                        autoSizeColumn: true,
-                        editor: {xtype: 'textfield'}
+        Ext.apply(this, {
+            items: [
+                {
+                    xtype: 'grid',
+                    loadMask: true,
+                    selModel: {
+                        selType: 'checkboxmodel', //'Ext.selection.CheckboxModel'
+                        checkOnly: true,
+                        mode: 'MULTI'
                     },
-                    {
-                        text: 'Status', dataIndex: 'status_name',
-                        minWidth: 100,
-                        autoSizeColumn: true,
-                        editor: {
-                            xtype: 'combobox',
-                            store: Ext.create('A.store.Status'),
-                            autoScroll: true,
-                            autoShow: true,
-                            forceSelection: true,
-                            queryMode: 'remote',
-                            displayField: 'name',
-                            valueField: 'name'
+                    store: store,
+                    columns: [
+                        //new Ext.grid.RowNumberer(),
+                        {text: 'ID', dataIndex: 'id', minWidth: 50, autoSizeColumn: true},
+                        {
+                            text: 'Name',
+                            dataIndex: 'name',
+                            minWidth: 100,
+                            autoSizeColumn: true,
+                            editor: {xtype: 'textfield'}
+                        },
+                        {
+                            text: 'Status', dataIndex: 'status_name',
+                            minWidth: 100,
+                            autoSizeColumn: true,
+                            editor: {
+                                xtype: 'combobox',
+                                store: Ext.create('A.store.Status'),
+                                autoScroll: true,
+                                autoShow: true,
+                                forceSelection: true,
+                                queryMode: 'remote',
+                                displayField: 'name',
+                                valueField: 'name'
+                            }
+                        },
+                        {
+                            text: 'Notes', dataIndex: 'notes', flex: 1,
+                            editor: {xtype: 'textfield'}
+                        },
+                        {
+                            xtype: 'actioncolumn',
+                            width: 30,
+                            align: 'center',
+                            todo: 'edit',
+                            items: [
+                                {
+                                    xtype: 'button',
+                                    icon: 'img/icons/essential/png/edit.png',
+                                    iconCls: 'icon-bg',
+                                    tooltip: 'Edit',
+                                }
+                            ]
+                        },
+                        {
+                            xtype: 'actioncolumn',
+                            width: 30,
+                            align: 'center',
+                            todo: 'delete',
+                            items: [
+                                {
+                                    xtype: 'button',
+                                    icon: 'img/icons/essential/png/trash.png',
+                                    iconCls: 'icon-bg',
+                                    tooltip: 'Delete'
+                                }
+                            ]
                         }
+                    ],
+                    plugins: [
+                        {
+                            ptype: 'cellediting', //'Ext.grid.plugin.CellEditing'
+                            clicksToEdit: 1
+                        }
+                    ],
+                    viewConfig: {
+                        trackOver: false,
+                        stripeRows: true,
                     },
-                    {
-                        text: 'Notes', dataIndex: 'notes', flex: 1,
-                        editor: {xtype: 'textfield'}
-                    },
-                    {
-                        xtype: 'actioncolumn',
-                        width: 30,
-                        align: 'center',
-                        todo: 'edit',
-                        items: [
-                            {
-                                xtype: 'button',
-                                icon: 'img/icons/essential/png/edit.png',
-                                iconCls: 'icon-bg',
-                                tooltip: 'Edit',
-                            }
-                        ]
-                    },
-                    {
-                        xtype: 'actioncolumn',
-                        width: 30,
-                        align: 'center',
-                        todo: 'delete',
-                        items: [
-                            {
-                                xtype: 'button',
-                                icon: 'img/icons/essential/png/trash.png',
-                                iconCls: 'icon-bg',
-                                tooltip: 'Delete'
-                            }
-                        ]
-                    }
-                ],
-                plugins: [
-                    {
-                        ptype: 'cellediting', //'Ext.grid.plugin.CellEditing'
-                        clicksToEdit: 1
-                    }
-                ],
-                viewConfig: {
-                    trackOver: false,
-                    stripeRows: true,
-                },
-                dockedItems: [
-                    {
-                        xtype: 'pagingtoolbar',
-                        store: store,
-                        dock: 'bottom',
-                        displayInfo: true,
-                        displayMsg: 'Displaying data {0} - {1} of {2}',
-                        emptyMsg: 'No data to display',
-                        inputItemWidth: 50
-                    },
-                    {
-                        xtype: 'toolbar',
-                        dock: 'top',
-                        items: [
-                            {
-                                xtype: 'button',
-                                icon: 'img/icons/essential/png/add-1.png',
-                                iconCls: 'icon-bg',
-                                text: 'New',
-                                todo: 'add',
-                                tooltip: 'Add new',
-                            },
-                            '->',
-                            {
-                                xtype: 'button',
-                                icon: 'img/icons/essential/png/trash.png',
-                                iconCls: 'icon-bg',
-                                text: 'Delete',
-                                todo: 'delete',
-                                tooltip: 'Delete selection',
-                            },
-                            {
-                                xtype: 'button',
-                                icon: 'img/icons/essential/png/save.png',
-                                iconCls: 'icon-bg',
-                                text: 'Save',
-                                todo: 'save',
-                                tooltip: 'Save selection',
-                            }
-                        ]
-                    }
-                ]
-            }
-        ]
-    });
-})();
+                    dockedItems: [
+                        {
+                            xtype: 'pagingtoolbar',
+                            store: store,
+                            dock: 'bottom',
+                            displayInfo: true,
+                            displayMsg: 'Displaying data {0} - {1} of {2}',
+                            emptyMsg: 'No data to display',
+                            inputItemWidth: 50
+                        },
+                        {
+                            xtype: 'toolbar',
+                            dock: 'top',
+                            items: [
+                                {
+                                    xtype: 'button',
+                                    icon: 'img/icons/essential/png/add-1.png',
+                                    iconCls: 'icon-bg',
+                                    text: 'New',
+                                    todo: 'add',
+                                    tooltip: 'Add new',
+                                },
+                                '->',
+                                {
+                                    xtype: 'button',
+                                    icon: 'img/icons/essential/png/trash.png',
+                                    iconCls: 'icon-bg',
+                                    text: 'Delete',
+                                    todo: 'delete',
+                                    tooltip: 'Delete selection',
+                                },
+                                {
+                                    xtype: 'button',
+                                    icon: 'img/icons/essential/png/save.png',
+                                    iconCls: 'icon-bg',
+                                    text: 'Save',
+                                    todo: 'save',
+                                    tooltip: 'Save selection',
+                                }
+                            ]
+                        }
+                    ]
+                }
+            ]
+        });
+
+        this.callParent(arguments);
+    }
+});
--- app/view/master/Status.js
+++ app/view/master/Status.js
@@ -1,117 +1,124 @@
-(function () {
-    let store = Ext.create('A.store.Status');
+Ext.require([
+    'A.store.Status'
+]);
+Ext.define('A.view.master.Status', {
+    extend: 'Ext.panel.Panel',
+    alias: 'widget.masterStatus',
+    initComponent: function () {
+        let store = Ext.create('A.store.Status');
 
-    return Ext.define('A.view.master.Status', {
-        extend: 'Ext.panel.Panel',
-        alias: 'widget.masterStatus',
-        items: [
-            {
-                store,
-                xtype: 'grid',
-                loadMask: true,
-                selModel: {
-                    selType: 'checkboxmodel', //'Ext.selection.CheckboxModel'
-                    checkOnly: true,
-                    mode: 'MULTI'
-                },
-                columns: [
-                    //new Ext.grid.RowNumberer(),
-                    {text: 'ID', dataIndex: 'id', minWidth: 50, autoSizeColumn: true},
-                    {
-                        text: 'Name',
-                        dataIndex: 'name',
-                        autoSizeColumn: true,
-                        minWidth: 100,
-                        editor: {xtype: 'textfield'}
+        Ext.apply(this, {
+            items: [
+                {
+                    xtype: 'grid',
+                    loadMask: true,
+                    selModel: {
+                        selType: 'checkboxmodel', //'Ext.selection.CheckboxModel'
+                        checkOnly: true,
+                        mode: 'MULTI'
                     },
-                    {
-                        text: 'Notes',
-                        dataIndex: 'notes',
-                        flex: 1,
-                        editor: {xtype: 'textfield'}
+                    store: store,
+                    columns: [
+                        //new Ext.grid.RowNumberer(),
+                        {text: 'ID', dataIndex: 'id', minWidth: 50, autoSizeColumn: true},
+                        {
+                            text: 'Name',
+                            dataIndex: 'name',
+                            autoSizeColumn: true,
+                            minWidth: 100,
+                            editor: {xtype: 'textfield'}
+                        },
+                        {
+                            text: 'Notes',
+                            dataIndex: 'notes',
+                            flex: 1,
+                            editor: {xtype: 'textfield'}
+                        },
+                        {
+                            xtype: 'actioncolumn',
+                            width: 30,
+                            align: 'center',
+                            todo: 'edit',
+                            items: [
+                                {
+                                    xtype: 'button',
+                                    icon: 'img/icons/essential/png/edit.png',
+                                    iconCls: 'icon-bg',
+                                    tooltip: 'Edit',
+                                }
+                            ]
+                        },
+                        {
+                            xtype: 'actioncolumn',
+                            width: 30,
+                            align: 'center',
+                            todo: 'delete',
+                            items: [
+                                {
+                                    xtype: 'button',
+                                    icon: 'img/icons/essential/png/trash.png',
+                                    iconCls: 'icon-bg',
+                                    tooltip: 'Delete'
+                                }
+                            ]
+                        }
+                    ],
+                    plugins: [
+                        {
+                            ptype: 'cellediting', //'Ext.grid.plugin.CellEditing'
+                            clicksToEdit: 1
+                        }
+                    ],
+                    viewConfig: {
+                        trackOver: false,
+                        stripeRows: true,
                     },
-                    {
-                        xtype: 'actioncolumn',
-                        width: 30,
-                        align: 'center',
-                        todo: 'edit',
-                        items: [
-                            {
-                                xtype: 'button',
-                                icon: 'img/icons/essential/png/edit.png',
-                                iconCls: 'icon-bg',
-                                tooltip: 'Edit',
-                            }
-                        ]
-                    },
-                    {
-                        xtype: 'actioncolumn',
-                        width: 30,
-                        align: 'center',
-                        todo: 'delete',
-                        items: [
-                            {
-                                xtype: 'button',
-                                icon: 'img/icons/essential/png/trash.png',
-                                iconCls: 'icon-bg',
-                                tooltip: 'Delete'
-                            }
-                        ]
-                    }
-                ],
-                plugins: [
-                    {
-                        ptype: 'cellediting', //'Ext.grid.plugin.CellEditing'
-                        clicksToEdit: 1
-                    }
-                ],
-                viewConfig: {
-                    trackOver: false,
-                    stripeRows: true,
-                },
-                dockedItems: [
-                    {
-                        xtype: 'pagingtoolbar',
-                        store: store,
-                        dock: 'bottom',
-                        displayInfo: true,
-                        displayMsg: 'Displaying data {0} - {1} of {2}',
-                        emptyMsg: 'No data to display',
-                        inputItemWidth: 50
-                    },
-                    {
-                        xtype: 'toolbar',
-                        dock: 'top',
-                        items: [
-                            {
-                                xtype: 'button',
-                                icon: 'img/icons/essential/png/add-1.png',
-                                iconCls: 'icon-bg',
-                                text: 'New',
-                                todo: 'add',
-                                tooltip: 'Add new',
-                            },
-                            '->',
-                            {
-                                xtype: 'button',
-                                icon: 'img/icons/essential/png/trash.png',
-                                iconCls: 'icon-bg',
-                                text: 'Delete',
-                                todo: 'delete',
-                                tooltip: 'Delete selection',
-                            },
-                            {
-                                xtype: 'button',
-                                icon: 'img/icons/essential/png/save.png',
-                                iconCls: 'icon-bg',
-                                text: 'Save',
-                                todo: 'save',
-                                tooltip: 'Save selection',
-                            }
-                        ]
-                    }
-                ]
-            }
-        ]
-    })
-})();
+                    dockedItems: [
+                        {
+                            xtype: 'pagingtoolbar',
+                            store: store,
+                            dock: 'bottom',
+                            displayInfo: true,
+                            displayMsg: 'Displaying data {0} - {1} of {2}',
+                            emptyMsg: 'No data to display',
+                            inputItemWidth: 50
+                        },
+                        {
+                            xtype: 'toolbar',
+                            dock: 'top',
+                            items: [
+                                {
+                                    xtype: 'button',
+                                    icon: 'img/icons/essential/png/add-1.png',
+                                    iconCls: 'icon-bg',
+                                    text: 'New',
+                                    todo: 'add',
+                                    tooltip: 'Add new',
+                                },
+                                '->',
+                                {
+                                    xtype: 'button',
+                                    icon: 'img/icons/essential/png/trash.png',
+                                    iconCls: 'icon-bg',
+                                    text: 'Delete',
+                                    todo: 'delete',
+                                    tooltip: 'Delete selection',
+                                },
+                                {
+                                    xtype: 'button',
+                                    icon: 'img/icons/essential/png/save.png',
+                                    iconCls: 'icon-bg',
+                                    text: 'Save',
+                                    todo: 'save',
+                                    tooltip: 'Save selection',
+                                }
+                            ]
+                        }
+                    ]
+                }
+            ]
+        });
+
+        this.callParent(arguments)
+    }
+});
