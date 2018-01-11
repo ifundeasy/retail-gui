c2478b6006db6e160b33e821250460fc80c140ef
Afa Rappresent <mail.vudin@gmail.com>
2018-01-11 13:53:11 +0700

    good state for master.media module

--- app/controller/master/Media.js
+++ app/controller/master/Media.js
@@ -2,9 +2,20 @@ Ext.define('A.controller.master.Media', {
     extend: 'Ext.app.Controller',
     views: ['master.Media'],
     refs: [
-        {ref: 'myGrid', selector: 'masterMedia grid'}
+        {ref: 'myGrid', selector: 'masterMedia grid'},
+        {ref: 'mySearchField', selector: 'masterMedia checkboxlistcombo'},
+        {ref: 'mySearchValue', selector: 'masterMedia textfield[todo=valueFilter]'}
     ],
+    ready4Filter: true,
     events: {
+        'masterMedia textfield[todo=valueFilter]': {
+            //change: 'filterGrid',
+            specialkey: 'pressedEnter'
+        },
+        'masterMedia checkboxlistcombo': {
+            //change: 'filterGrid',
+            afterrender: 'addedSearchField'
+        },
         'masterMedia grid': {
             afterrender: 'addedGrid',
             deselect: 'deselectRow',
@@ -29,44 +40,137 @@ Ext.define('A.controller.master.Media', {
             click: 'saveRows'
         }
     },
+    pressedEnter: function (cmp, e) {
+        if (e.keyCode === 13) this.filterGrid();
+    },
+    filterGrid: async function () {
+        let me = this;
+
+        if (!me.ready4Filter) return;
+        me.ready4Filter = false;
+
+        let store = me.getMyGrid().getStore();
+        let fields = me.getMySearchField().getValue() || [];
+        let value = me.getMySearchValue().getValue();
+        let $or = [];
+
+        fields.forEach(function (el) {
+            if (el !== -1) $or.push({[el]: {$like: `%${value}%`}})
+        });
+
+        store.proxy.extraParams = value && $or.length ? {filter: JSON.stringify({$or})} : null;
+
+        await store.Load();
+        me.ready4Filter = true;
+    },
+    addedSearchField: function () {
+        let keyValueStore = Ext.create('A.store.KeyValue');
+        let columns = this.getMyGrid().columns;
+        let searchField = this.getMySearchField();
+        keyValueStore.insert(0, {key: -1, value: 'All'});
+        columns.forEach(function (col, i) {
+            if (col.dataIndex) {
+                keyValueStore.insert(i + 1, {key: col.dataIndex, value: col.text})
+            }
+        });
+        searchField.bindStore(keyValueStore);
+    },
     refreshView: function (dataview) {
         if (dataview.panel) {
             Ext.each(dataview.panel.columns, function (column) {
-                if (column.autoSizeColumn === true)
+                if (column.autoSizeColumn === true) {
                     column.autoSize();
+                }
             })
         }
 
     },
     addRow: function () {
-        console.log('masterMedia addRow')
+        let grid = this.getMyGrid();
+        let store = grid.getStore();
+        let rec = store.insert(0, {});
+        grid.plugins[0].startEditByPosition({
+            row: rec[0],
+            column: 2
+        });
     },
-    editRow: function (grid, rowIndex, colIndex, item, e, record) {
-        console.log('masterMedia editRow')
+    editRow: function (gridview, rowIndex, colIndex, item, e, record) {
+        this.getMyGrid().plugins[0].startEditByPosition({
+            row: record,
+            column: 2
+        });
     },
-    deleteRow: function (grid, rowIndex, colIndex, item, e, record) {
-        console.log('masterMedia deleteRow')
+    deleteRow: function (gridview, rowIndex, colIndex, item, e, record) {
+        let grid = this.getMyGrid();
+        let store = grid.getStore();
+        Ext.Msg.show({
+            title: 'Confirm',
+            msg: 'Deleting an item. This action will take effect when you save a data.',
+            buttons: Ext.MessageBox.YESNO,
+            closeable: false,
+            icon: Ext.Msg.QUESTION,
+            animateTarget: grid,
+            fn: function (choose) {
+                if (choose === 'yes') {
+                    store.remove(record);
+                }
+            }
+        });
     },
-    deleteRows: function () {
-        console.log('masterMedia deleteRows')
+    deleteRows: function (cmp) {
+        let grid = this.getMyGrid();
+        let {items} = grid.getSelectionModel().selected;
+        if (items.length) {
+            Ext.Msg.show({
+                title: 'Confirm',
+                msg: 'Delete multiple, ' + items.length + ' items. This action will take effect when you save a data.',
+                buttons: Ext.MessageBox.YESNO,
+                closeable: false,
+                icon: Ext.Msg.QUESTION,
+                animateTarget: cmp,
+                fn: function (choose) {
+                    if (choose === 'yes') {
+                        grid.getStore().remove(items);
+                    }
+                }
+            });
+        }
+    },
+    saveRows: function (cmp) {
+        let grid = this.getMyGrid();
+        let store = grid.getStore();
+        let msg = [], op = { create: 0, delete: 0, update: 0 };
+        op.delete += store.removed.length;
+        store.each(function (rec) {
+            if (!rec.raw.id) op.create += 1;
+            if (rec.raw.id && rec.dirty) op.update += 1;
+        });
+        for (let o in op) if (op[o]) msg.push(o + ' ' + op[o] + ' items');
+        Ext.Msg.show({
+            title: 'Confirm',
+            msg: 'Operation such : ' + msg.join(', ') + ' will be save.<br/>Confirm for saving operation.',
+            buttons: Ext.MessageBox.YESNO,
+            closeable: false,
+            icon: Ext.Msg.QUESTION,
+            animateTarget: cmp,
+            fn: async function (choose) {
+                if (choose === 'yes') {
+                    let mySync = await store.Sync();
+                    if (mySync instanceof Error) {
+                        console.log(mySync)
+                    } else {
+                        console.log('SUCCESS', mySync);
+                        await store.Load();
+                    }
+                }
+            }
+        });
     },
     deselectRow: function (model, record, index) {
         console.log('masterMedia deselectRow')
     },
     selectRow: function (model, record, index) {
         console.log('masterMedia selectRow')
-    },
-    saveRows: async function () {
-        let me = this;
-        let grid = me.getMyGrid();
-        let store = grid.getStore();
-        let mySync = await store.Sync();
-        if (mySync instanceof Error) {
-            console.log(mySync)
-        } else {
-            console.log('SUCCESS', mySync);
-            await store.Load();
-        }
     },
     addedGrid: async function () {
         let me = this;
--- app/view/master/Media.js
+++ app/view/master/Media.js
@@ -1,16 +1,20 @@
 Ext.require([
+    'A.ux.CheckboxListCombo',
     'A.store.Media',
     'A.store.Status',
+    'A.store.Table',
 ]);
 Ext.define('A.view.master.Media', {
     extend: 'Ext.panel.Panel',
     alias: 'widget.masterMedia',
     initComponent: function () {
         let store = Ext.create('A.store.Media');
+        let statusStore = Ext.create('A.store.Status');
 
         Ext.apply(this, {
             items: [
                 {
+                    id: 'xx',
                     xtype: 'grid',
                     loadMask: true,
                     selModel: {
@@ -30,18 +34,53 @@ Ext.define('A.view.master.Media', {
                             editor: {xtype: 'textfield'}
                         },
                         {
-                            text: 'Status', dataIndex: 'status_name',
+                            text: 'Table',
+                            dataIndex: 'TABLENAME',
                             minWidth: 100,
                             autoSizeColumn: true,
                             editor: {
                                 xtype: 'combobox',
-                                store: Ext.create('A.store.Status'),
+                                store: Ext.create('A.store.Table'),
                                 autoScroll: true,
                                 autoShow: true,
                                 forceSelection: true,
                                 queryMode: 'remote',
                                 displayField: 'name',
                                 valueField: 'name'
+                            }
+                        },
+                        {
+                            text: 'For ID',
+                            dataIndex: 'TABLENAME_id',
+                            minWidth: 100,
+                            autoSizeColumn: true,
+                            editor: {
+                                xtype: 'numberfield',
+                                minValue: 1
+                            }
+                        },
+                        {
+                            text: 'Status',
+                            dataIndex: 'status_id',
+                            minWidth: 100,
+                            autoSizeColumn: true,
+                            renderer: function(val, meta, record, rowIndex) {
+                                if (!record.dirty) return record.data.status_name;
+                                let idx = statusStore.findBy(function (rec) {
+                                    if (rec.data.id == val) return 1;
+                                    return 0
+                                });
+                                return statusStore.getAt(idx).get('name')
+                            },
+                            editor: {
+                                xtype: 'combobox',
+                                store: statusStore,
+                                autoScroll: true,
+                                autoShow: true,
+                                forceSelection: true,
+                                queryMode: 'remote',
+                                displayField: 'name',
+                                valueField: 'id'
                             }
                         },
                         {
@@ -109,7 +148,6 @@ Ext.define('A.view.master.Media', {
                                     todo: 'add',
                                     tooltip: 'Add new',
                                 },
-                                '->',
                                 {
                                     xtype: 'button',
                                     icon: 'img/icons/essential/png/trash.png',
@@ -125,6 +163,24 @@ Ext.define('A.view.master.Media', {
                                     text: 'Save',
                                     todo: 'save',
                                     tooltip: 'Save selection',
+                                },
+                                '->',
+                                {
+                                    xtype: 'checkboxlistcombo',
+                                    displayField: 'value',
+                                    valueField: 'key',
+                                    editable: false,
+                                    fieldLabel: 'Search by ',
+                                    labelWidth: 70,
+                                    multiSelect: true,
+                                    firstItemChecksAll: true
+                                },
+                                {
+                                    xtype: 'textfield',
+                                    text: 'Search value',
+                                    todo: 'valueFilter',
+                                    width: 256,
+                                    tooltip: 'Value filter',
                                 }
                             ]
                         }
