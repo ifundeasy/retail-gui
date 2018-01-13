d44c8fd9f3c9cc65575433a999786e2a9ee7720a
Afa Rappresent <mail.vudin@gmail.com>
2018-01-13 18:21:22 +0700

    ux for suggetion input field, from combobox's extend

---
+++ app/other/SuggestBox.js
@@ -0,0 +1,46 @@
+Ext.define('A.other.SuggestBox', {
+    extend: 'Ext.form.field.ComboBox',
+    alias: 'widget.suggestbox',
+    displayField: 'name',
+    valueField: 'id',
+    minChars: 2,
+    anyMatch: true,
+    autoShow: true,
+    autoSelect: true,
+    typeAhead: true,
+    selectOnFocus: true,
+    queryMode: 'remote',
+    queryParam: 'filter',
+    triggerAction: 'all',
+    maxLength: 100,
+    enforceMaxLength: true,
+    autoRender: true,
+    grow: true,
+    growMin: 100,
+    //pageSize: 20,
+    //growMin: 323, //if using pageSize make sure growMin value is 323, it's make your pagingToolbar fit with suggestion box
+    //maxWidth: 200, //use maxWidth for restrict input box width constraint instead suggestion box
+    listeners: {
+        afterrender: function () {
+            this.growMin += 20;
+            this.listConfig = {
+                itemTpl: `{${this.displayField}} <i style="color: rgba(0,0,0,0.2)">(id: {${this.valueField}})</i>`
+            }
+        },
+        focus: function (cmp) {
+            if (this.secondTime) return;
+
+            let store = cmp.getStore();
+            let key1 = this.displayField;
+            let key2 = this.valueField;
+            this.secondTime = true;
+            store.on('beforeload', function(store, eOpts){
+                let {filter} = eOpts.params;
+                eOpts.params.filter = JSON.stringify({$or: [
+                    {[key1]: {$like : `%${filter}%`}},
+                    {[key2]: {$like : `%${filter}%`}}
+                ]});
+            });
+        }
+    }
+});
