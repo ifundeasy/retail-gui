d7f8113757050ac47aebb01c328ac6b610454867
Afa Rappresent <mail.vudin@gmail.com>
2018-01-14 20:58:13 +0700

    good state for master.type module

--- app/view/master/Unit.js
+++ app/view/master/Unit.js
@@ -1,3 +1,166 @@
+Ext.require([
+    'A.store.Unit',
+    'A.store.Status'
+]);
 Ext.define('A.view.master.Unit', {
-    extend: 'Ext.panel.Panel'
+    extend: 'Ext.panel.Panel',
+    alias: 'widget.masterUnit',
+    initComponent: function () {
+        let store = Ext.create('A.store.Unit');
+        let statusStore = Ext.create('A.store.Status');
+
+        Ext.apply(this, {
+            items: [
+                {
+                    xtype: 'grid',
+                    loadMask: true,
+                    selModel: {
+                        selType: 'checkboxmodel', //'Ext.selection.CheckboxModel'
+                        checkOnly: true,
+                        mode: 'MULTI'
+                    },
+                    store: store,
+                    columns: [
+                        //new Ext.grid.RowNumberer(),
+                        {text: 'ID', dataIndex: 'id', minWidth: 50, autoSizeColumn: true},
+                        {
+                            text: 'Code',
+                            dataIndex: 'short',
+                            minWidth: 100,
+                            autoSizeColumn: true,
+                            editor: {xtype: 'textfield', minValue:1}
+                        },
+                        {
+                            text: 'Name',
+                            dataIndex: 'name',
+                            minWidth: 100,
+                            autoSizeColumn: true,
+                            editor: {xtype: 'textfield'}
+                        },
+                        {
+                            text: 'Status',
+                            dataIndex: 'status_id',
+                            minWidth: 100,
+                            autoSizeColumn: true,
+                            renderer: function(val, meta, record, rowIndex) {
+                                if (!record.dirty) return record.data.status_name || '';
+                                let idx = statusStore.findExact('id', val);
+                                return (idx === -1) ? '' : statusStore.getAt(idx).get('name');
+                            },
+                            editor: {
+                                xtype: 'suggestbox',
+                                displayField: 'name',
+                                valueField: 'id',
+                                editable: false,
+                                store: statusStore
+                            }
+                        },
+                        {
+                            text: 'Notes', dataIndex: 'notes', flex: 1,
+                            editor: {xtype: 'textfield'}
+                        },
+                        {
+                            xtype: 'actioncolumn',
+                            width: 30,
+                            align: 'center',
+                            todo: 'edit',
+                            items: [
+                                {
+                                    xtype: 'button',
+                                    icon: 'img/icons/essential/png/edit.png',
+                                    iconCls: 'icon-bg',
+                                    tooltip: 'Edit',
+                                }
+                            ]
+                        },
+                        {
+                            xtype: 'actioncolumn',
+                            width: 30,
+                            align: 'center',
+                            todo: 'delete',
+                            items: [
+                                {
+                                    xtype: 'button',
+                                    icon: 'img/icons/essential/png/trash.png',
+                                    iconCls: 'icon-bg',
+                                    tooltip: 'Delete'
+                                }
+                            ]
+                        }
+                    ],
+                    plugins: [
+                        {
+                            ptype: 'cellediting', //'Ext.grid.plugin.CellEditing'
+                            clicksToEdit: 1
+                        }
+                    ],
+                    viewConfig: {
+                        trackOver: false,
+                        stripeRows: true,
+                    },
+                    dockedItems: [
+                        {
+                            xtype: 'pagingtoolbar',
+                            store: store,
+                            dock: 'bottom',
+                            displayInfo: true,
+                            displayMsg: 'Displaying data {0} - {1} of {2}',
+                            emptyMsg: 'No data to display',
+                            inputItemWidth: 50
+                        },
+                        {
+                            xtype: 'toolbar',
+                            dock: 'top',
+                            items: [
+                                {
+                                    xtype: 'button',
+                                    icon: 'img/icons/essential/png/add-1.png',
+                                    iconCls: 'icon-bg',
+                                    text: 'New',
+                                    todo: 'add',
+                                    tooltip: 'Add new',
+                                },
+                                {
+                                    xtype: 'button',
+                                    icon: 'img/icons/essential/png/trash.png',
+                                    iconCls: 'icon-bg',
+                                    text: 'Delete',
+                                    todo: 'delete',
+                                    tooltip: 'Delete selection',
+                                },
+                                {
+                                    xtype: 'button',
+                                    icon: 'img/icons/essential/png/save.png',
+                                    iconCls: 'icon-bg',
+                                    text: 'Save',
+                                    todo: 'save',
+                                    tooltip: 'Save selection',
+                                },
+                                '->',
+                                {
+                                    xtype: 'checkboxlistcombo',
+                                    displayField: 'value',
+                                    valueField: 'key',
+                                    editable: false,
+                                    fieldLabel: 'Search by ',
+                                    labelWidth: 70,
+                                    multiSelect: true,
+                                    firstItemChecksAll: true
+                                },
+                                {
+                                    xtype: 'textfield',
+                                    text: 'Search value',
+                                    todo: 'valueFilter',
+                                    width: 256,
+                                    tooltip: 'Value filter',
+                                }
+                            ]
+                        }
+                    ]
+                }
+            ]
+        });
+
+        this.callParent(arguments);
+    }
 });
