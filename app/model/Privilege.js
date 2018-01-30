4ff5edf9e37c5908188b11cdd5edf320b373f525
Afa Rappresent <mail.vudin@gmail.com>
2018-01-31 03:47:04 +0700

    add privilege model

---
+++ app/model/Privilege.js
@@ -0,0 +1,37 @@
+Ext.define('A.model.Privilege', {
+    extend: 'Ext.data.Model',
+    pathURL: '/api/actorModule',
+    fields: [
+        {name: 'id', type: 'int'},
+        {name: 'actor_id', type: 'int'},
+        {name: 'actor_name', type: 'string'},
+        {name: 'actor_status_id', type: 'int'},
+        {name: 'actor_status_name', type: 'string'},
+        {name: 'actor_status_notes', type: 'string'},
+        {name: 'module_id', type: 'int'},
+        {name: 'module_module_id', type: 'int'},
+        {name: 'module_name', type: 'string'},
+        {name: 'module_class', type: 'string'},
+        {name: 'module_seq', type: 'int'},
+        {name: 'module_status_id', type: 'int'},
+        {name: 'module_notes', type: 'string'},
+        {name: 'status_id', type: 'int'},
+        {name: 'status_name', type: 'string'},
+        {name: 'status_notes', type: 'string'},
+        {name: 'notes', type: 'string'},
+        {
+            name: '_',
+            type: 'auto',
+            convert: function (val, rec) {
+                return {id: rec.raw.id}
+            }
+        }
+    ],
+    writer: [
+        {name: 'id', type: 'int'},
+        {name: 'actor_id', type: 'int'},
+        {name: 'module_id', type: 'int'},
+        {name: 'status_id', type: 'int'},
+        {name: 'notes', type: 'string'}
+    ]
+});
